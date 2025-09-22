2a55d0357f4e2f98e11bc0096d7d89817dbcd441
SpacePistache <lukeholley@hotmail.com>
2025-09-22 14:31:34 +0200

    I hate this

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
-"""A small script determining if all boxes can be opened"""
+"""A small script determining if all boxes (list of lists) can be opened"""
+
 
 def canUnlockAll(boxes):
-	for box in boxes:
-		if box == [0] in boxes:
-			print(boxes)
+    unlocked_box = [0]
+    checked_boxes = [0]
+    while checked_boxes:
+        current = checked_boxes.pop()
+        for box in boxes[current]:
+            if box < len(boxes) and not unlocked_box:
+                unlocked_box.append(box)
+                checked_boxes.append(box)
+    if len(unlocked_box) == len(boxes):
+        return True
+    else:
+        return False

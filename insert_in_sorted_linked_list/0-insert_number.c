c044cba375c24bf67f3d9ecc0b014564f148d1bf
SpacePistache <lukeholley@hotmail.com>
2025-09-23 13:35:19 +0200

    fixed some betty syntax errors

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -3,8 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-listint_t *insert_node(listint_t **head, int number) {
+listint_t *insert_node(listint_t **head, int number) 
+{
 	listint_t *new_node = malloc(sizeof(listint_t))
 	if (new_node == NULL)
 		return NULL;
-};
+}

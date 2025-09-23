0f7dbb267251ffcb904280f5e93f3190fbae1307
SpacePistache <lukeholley@hotmail.com>
2025-09-23 13:43:01 +0200

    fixed missing semi colons

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -1,13 +1,13 @@
-#include "main.h"
+#include <main.h>
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 listint_t *insert_node(listint_t **head, int number) 
 {
-	listint_t *new_node = malloc(sizeof(listint_t))
+	listint_t *new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return NULL;
 
-	return new_node
+	return new_node;
 }

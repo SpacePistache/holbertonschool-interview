4d51eda5490e0e5daea4be0db088872869f76f99
SpacePistache <lukeholley@hotmail.com>
2025-09-23 15:33:46 +0200

    added other files and tried compiling for debugguing puposes

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -1,4 +1,3 @@
-#include <main.h>
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,6 +7,8 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return NULL;
+
+	new_node -> n = number;
 
 	return new_node;
 }

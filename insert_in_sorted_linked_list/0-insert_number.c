319e3db74e1715a9d835fd4491abbc845031bcf0
SpacePistache <lukeholley@hotmail.com>
2025-09-24 09:53:31 +0200

    fixed some syntax errors

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-listint_t *insert_node(listint_t **head, int number) 
+listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 
-	new_node -> n = number;
+	new_node->n = number;
 
-	return new_node;
+	return (new_node);
 }

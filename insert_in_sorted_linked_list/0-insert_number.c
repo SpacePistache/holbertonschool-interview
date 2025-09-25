82f780d836b77e57825f142d03a154ab08249557
SpacePistache <lukeholley@hotmail.com>
2025-09-25 15:38:09 +0200

    added comments at start of file

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -2,9 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * insert_node - function to insert a node with value
+ * @head: A pointer to the start of the linked list
+ * @number: The value stored in new_node
+ * Return: the address of new node or NULL if failure
+*/
+
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
+
 	if (new_node == NULL)
 		return (NULL);
 

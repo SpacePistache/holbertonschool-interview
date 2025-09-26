61ed79099d8464da60927473c051910f22186b89
SpacePistache <lukeholley@hotmail.com>
2025-09-26 10:33:49 +0200

    added management of current node

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -12,13 +12,16 @@
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *current = *head;
 
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = number;
-	new_node->next = *head;
-	*head = new_node;
+	
+	while (current->next !=NULL) {
+		current = current->next;
+	}
 
 	return (new_node);
 }

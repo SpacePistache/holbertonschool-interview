0c59abde0b26e726cec5622ecfd4cb87010f244a
SpacePistache <lukeholley@hotmail.com>
2025-09-26 11:19:57 +0200

    dealt with temp pointer and head

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -18,10 +18,21 @@ listint_t *insert_node(listint_t **head, int number)
 		return (NULL);
 
 	new_node->n = number;
-	
-	while (current->next !=NULL) {
-		current = current->next;
+	new_node->next = NULL;
+
+	if (*head == NULL || (*head)->n >= number)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
+	current = *head;
+	while (current->next != NULL && current->next->n < number)
+	current = current->next;
+
+
+	new_node->next = current->next;
+	current->next = new_node;
 
 	return (new_node);
 }

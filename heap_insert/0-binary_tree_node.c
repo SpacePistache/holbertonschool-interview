bf99fce83bcd9ed273f567ac27c664f767323505
SpacePistache <lukeholley@hotmail.com>
2025-11-14 13:47:46 +0100

    added prototype to main.h file

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -16,7 +16,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
 
 	if (new_node == NULL) 
 		return NULL;
-	
+
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
--- heap_insert/0-main.c
+++ heap_insert/0-main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
+
 /**
  * _binary_tree_delete - Deallocate a binary tree
  *

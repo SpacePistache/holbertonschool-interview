65853e4ed1cb00d885ee151f3b44654af1667395
SpacePistache <lukeholley@hotmail.com>
2026-04-22 14:50:11 +0200

    reverted back

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Module that reads stdin line by line and computes metrics"""
 import sys
 
 
@@ -37,9 +38,9 @@ def main():
                 print_stats(total_size, status_counts)
 
     except KeyboardInterrupt:
+        pass
+    finally:
         print_stats(total_size, status_counts)
-        sys.stdout.flush()
-        sys.exit(0)
 
 
 if __name__ == "__main__":

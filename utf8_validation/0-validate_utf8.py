fee1449690383c9e9cd2f6912a22b82ca4c8be48
SpacePistache <lukeholley@hotmail.com>
2026-07-17 10:32:30 +0200

    initial commit utf8 vamidation

---
+++ utf8_validation/0-validate_utf8.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Module for validating UTF-8 encoding.
+"""
+
+
+def validUTF8(data):
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
+
+    Args:
+        data: List of integers representing bytes.
+
+    Returns:
+        True if data is a valid UTF-8 encoding, False otherwise.
+    """
+    num_bytes = 0
+
+    for byte in data:
+        byte &= 0xFF
+
+        if num_bytes == 0:
+            if (byte >> 7) == 0:
+                continue
+            elif (byte >> 5) == 0b110:
+                num_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                num_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                num_bytes = 3
+            else:
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            num_bytes -= 1
+
+    return num_bytes == 0
